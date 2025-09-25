08801ec64ab7dc6c8b0629dfba25eb84b6c28e0a
dwiki-mukti <243dwiki.com@gmail.com>
2025-09-25 23:13:28 +0700

    Add | init print pdf backend

--- app/tatib/_backend/controllers/PortofolioNonAcademicController.ts
+++ app/tatib/_backend/controllers/PortofolioNonAcademicController.ts
@@ -1,13 +1,41 @@
 import { Elysia } from "elysia";
 import { guardedUserAuthed, LoadUserAuthed } from "@/externals/utils/backend/src/auth-midleware";
-
+import puppeteer from 'puppeteer-core'
+import { Launcher } from "chrome-launcher"
 
 
 const PortofolioNonAcademicController = new Elysia()
   .use(LoadUserAuthed)
   .group('/portofolio-non-academic', (group) => (group.guard({ beforeHandle: guardedUserAuthed }, (app) => {
-    app.get('/pdf/download', async () => { 
+    app.get('/pdf/test', async ({ }) => {
+      return { message: 'oke' }
+    })
+    app.get('/pdf/download', async ({ set }) => {
+      const browser = await puppeteer.launch({
+        executablePath: Launcher.getInstallations()[0],
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
 
+
+      const page = await browser.newPage();
+
+      await page.setContent(`
+        <html>
+          <body>
+            <h1>Hello from Elysia & Puppeteer!</h1>
+            <p>This is a sample PDF document generated from HTML.</p>
+          </body>
+        </html>
+      `);
+
+      const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+      await browser.close();
+
+
+      set.headers['content-type'] = 'application/pdf';
+      set.headers['content-disposition'] = 'inline; filename="generated.pdf"';
+
+      return pdfBuffer;
     });
     // app.get('/pdf/download', async () => {
     //   const userProfile = {
---
+++ app/tatib/panel/portofolio-non-akademik/_pdf/PortofolioNonAcademic.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PortofolioNonAcademic() {
+  return (
+    <div>PortofolioNonAcademic</div>
+  )
+}
